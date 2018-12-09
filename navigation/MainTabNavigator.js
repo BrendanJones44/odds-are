13581679a2aae523fd1de74aaf0203e0459759c3
BrendanJones44 <bpj1651@rit.edu>
2018-12-08 23:21:33 -0500

    add material-ui and replace bottom nav

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -1,29 +1,51 @@
 import React from 'react';
-import { Platform } from 'react-native';
-import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
-
-import TabBarIcon from '../components/TabBarIcon';
 import HomeScreen from '../screens/HomeScreen';
 
-const HomeStack = createStackNavigator({
-  Home: HomeScreen,
-});
-
-HomeStack.navigationOptions = {
-  tabBarLabel: 'Home',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={
-        Platform.OS === 'ios'
-          ? `ios-information-circle${focused ? '' : '-outline'}`
-          : 'md-information-circle'
-      }
-    />
-  ),
-};
-
-
-export default createBottomTabNavigator({
-  HomeStack,
-});
+import { BottomNavigation } from 'react-native-material-ui';
+import {
+  ScrollView,
+  StyleSheet,
+  TextInput,
+  Button,
+  View,
+} from 'react-native';
+export default class MainTabNavigator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { active: ''}
+  }
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <HomeScreen />
+      <View>
+      <BottomNavigation active={this.state.active} hidden={false} >
+        <BottomNavigation.Action
+          key="notifications"
+          icon="notifications"
+          label="(1)"
+          onPress={() => this.setState({ active: 'notifications' })}
+        />
+        <BottomNavigation.Action
+          key="add"
+          icon="add"
+          label="New"  
+          onPress={() => this.setState({ active: 'add' })}
+        />
+        <BottomNavigation.Action
+          key="list"
+          icon="list"
+          label="Past"
+          onPress={() => this.setState({ active: 'last' })}
+        />
+        <BottomNavigation.Action
+          key="settings"
+          icon="settings"
+          label="Settings"
+          onPress={() => this.setState({ active: 'settings' })}
+        />
+      </BottomNavigation>
+      </View>
+    </View>
+    )}
+}
